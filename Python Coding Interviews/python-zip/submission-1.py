2d92ced1dd7356ecf3079a543e625a1270bc3a03
snhabhi <55024554+snhabhi@users.noreply.github.com>
2026-08-16 12:00:04 +0530

    Add: python-zip - submission-1

---
+++ Python Coding Interviews/python-zip/submission-1.py
@@ -0,0 +1,19 @@
+from typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+    temp_dict= {}
+    for name, score in zip(names,scores):
+        temp_dict[name]=score
+    return temp_dict
+
+
+
+
+    #pass
+
+
+# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
