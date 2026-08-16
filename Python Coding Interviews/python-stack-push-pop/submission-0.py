6739f5b7ab1adbdf1351600b45038ce63695243e
snhabhi <55024554+snhabhi@users.noreply.github.com>
2026-08-16 16:00:27 +0530

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    result_list =arr[::-1]
+    return result_list
+    #pass
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
